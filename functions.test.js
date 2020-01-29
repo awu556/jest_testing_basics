01dc6394d54bdc72849a2b80f755f5f2556a4bf1
Wutang445 <austinw556@gmail.com>
2020-01-29 18:12:48 -0500

    added more describe blocks

--- functions.test.js
+++ functions.test.js
@@ -1,19 +1,25 @@
 const functions = require("./functions");
 
-test("Adds 2 + 2 to equal 4", () => {
-  expect(functions.add(2, 2)).toBe(4);
+describe("add", () => {
+  test("Adds 2 + 2 to equal 4", () => {
+    expect(functions.add(2, 2)).toBe(4);
+  });
+
+  test("Adds 2 + 2 to NOT equal 5", () => {
+    expect(functions.add(2, 2)).not.toBe(5);
+  });
 });
 
-test("Adds 2 + 2 to NOT equal 5", () => {
-  expect(functions.add(2, 2)).not.toBe(5);
+describe("isNull", () => {
+  test("Should return a null value", () => {
+    expect(functions.isNull()).toBeNull();
+  });
 });
 
-test("Should return a null value", () => {
-  expect(functions.isNull()).toBeNull();
-});
-
-test("Should check if the value is falsy", () => {
-  expect(functions.checkValue(null)).toBeFalsy();
+describe("checkValue", () => {
+  test("Should check if the value is falsy", () => {
+    expect(functions.checkValue(null)).toBeFalsy();
+  });
 });
 
 describe("createUser", () => {
